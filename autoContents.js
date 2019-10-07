fe16f55b7b65bad61d8c7693577ea14340d6998c
gobackk1 <k.kwbt4@icloud.com>
2019-10-07 15:39:29 +0900

    [fix] amendの練習 diff --git a/autoContents.js b/autoContents.js index 1b46969..77a66a8 100644 --- a/autoContents.js +++ b/autoContents.js @@ -2,10 +2,11 @@    $(window).on('load', function () {      //目次自動生成      var $headlines = $('.blocks h2');//目次にしたいh2 -    var $subHeadlines = $('.blocks h3'); -    var $contentsPosition = $('.info-list');//この要素の上に目次を挿入する +    var $subHeadlines = $('.blocks h3');//目次にしたいh3 +    var $contentsPosition = $('.single-ttl-bg');//この要素の下に目次を挿入する      var headlinesLength = $headlines.length;      var subHeadlinesLength = $subHeadlines.length; +    var subHeadCount = 0;
         initContents();
    
    @@ -32,15 +33,18 @@
             if (h3.length) {
               var subFrag = $(document.createDocumentFragment());
               var subList = $('<ul>');
    -          for (var j = 0; j < subHeadlinesLength; j++) {
    +          for (var j = 0; j < h3.length; j++) {
                 var subListItem = $('<li></li>');
    -            var subText = $subHeadlines[j].textContent;
    +            var subText = h3.get(j).textContent;
    +            console.log(h3.get(j).textContent);
    +
                 var subAnchor = $('<a></a>', {
    -              href: '#a_' + j,
    +              href: '#a_' + subHeadCount,
                   text: subText
                 });
                 subListItem.append(subAnchor);
                 subFrag.append(subListItem);
    +            subHeadCount++;
               }
               subList.append(subFrag);
               listItem.append(subList);
    @@ -50,6 +54,15 @@
           list.append(frag);
           contentsList.append(title).append(list);
           $contentsPosition.after(contentsList);
    +      $('.contents-list').on('click', 'a[href^="#"]', function () {
    +        var speed = 400;
    +        var href = $(this).attr("href");
    +        var target = $(href == "#" || href == "" ? 'html' : href);
    +        var position = target.offset().top;
    +        $('body,html').animate({ scrollTop: position }, speed, 'swing');
    +        history.pushState(null, null, href);
    +        return false;
    +      });
         }
    
         //目次にしたタイトルにアンカーをつける

--- autoContents.js
+++ autoContents.js
@@ -2,10 +2,11 @@
   $(window).on('load', function () {
     //目次自動生成
     var $headlines = $('.blocks h2');//目次にしたいh2
-    var $subHeadlines = $('.blocks h3');
-    var $contentsPosition = $('.info-list');//この要素の上に目次を挿入する
+    var $subHeadlines = $('.blocks h3');//目次にしたいh3
+    var $contentsPosition = $('.single-ttl-bg');//この要素の下に目次を挿入する
     var headlinesLength = $headlines.length;
     var subHeadlinesLength = $subHeadlines.length;
+    var subHeadCount = 0;
 
     initContents();
 
@@ -32,15 +33,18 @@
         if (h3.length) {
           var subFrag = $(document.createDocumentFragment());
           var subList = $('<ul>');
-          for (var j = 0; j < subHeadlinesLength; j++) {
+          for (var j = 0; j < h3.length; j++) {
             var subListItem = $('<li></li>');
-            var subText = $subHeadlines[j].textContent;
+            var subText = h3.get(j).textContent;
+            console.log(h3.get(j).textContent);
+
             var subAnchor = $('<a></a>', {
-              href: '#a_' + j,
+              href: '#a_' + subHeadCount,
               text: subText
             });
             subListItem.append(subAnchor);
             subFrag.append(subListItem);
+            subHeadCount++;
           }
           subList.append(subFrag);
           listItem.append(subList);
@@ -50,6 +54,15 @@
       list.append(frag);
       contentsList.append(title).append(list);
       $contentsPosition.after(contentsList);
+      $('.contents-list').on('click', 'a[href^="#"]', function () {
+        var speed = 400;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $('body,html').animate({ scrollTop: position }, speed, 'swing');
+        history.pushState(null, null, href);
+        return false;
+      });
     }
 
     //目次にしたタイトルにアンカーをつける
